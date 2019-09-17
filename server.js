42f214fbcb511302d39c7d04b11997e253c660d0
Casper Bartholomaus <casperbartholomaus@hotmail.com>
2019-09-17 08:48:55 +0200

    Added cross-env and dotenv package for environment variables and create config.js

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PACKAGE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+require("dotenv").config(); //Sets up dotenv as soon as the applications starts
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CONFIGURATION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 const app = express();
+const environment = process.env.NODE_ENV;
+const stage = require("./config")[environment];
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
 }));
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ROUTES ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 require("./routes")(app);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SERVER ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-app.listen(3000, () => {
+app.listen(stage.port, () => {
     console.log("Server listening on port: 3000");
 });
